74d4c4d77dcff3c568e4a9ed320b739f09360d27
richardMRa <richete05@gmail.com>
2025-07-02 21:12:17 -0500

    [add] code comments and basic middleware error handling

--- server/server.js
+++ server/server.js
@@ -1,19 +1,25 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Middlewares
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+// Basic route
+app.get("/", (req, res) => {
+  res.json({ message: "Login API is active" });
+});
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Login API is active'})
-})
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong!"})
+});
 
-
+// Start server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
